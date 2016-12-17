1e9115868f92357b7193603c0dff61ce0d7ebd7c
sunlight833 <484390775@qq.com>
2016-12-18 00:46:37 +0800

    header finished

--- src/components/title/title.vue
+++ src/components/title/title.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="title">
+  <div class="detail-title">
     <div class="line"></div>
     <div class="text">{{ title }}</div>
     <div class="line"></div>
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-.title
+.detail-title
   display: flex
   width: 80%
   margin: 28px auto 24px auto
